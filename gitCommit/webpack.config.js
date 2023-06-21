b22d7c1c996ba505814d248102471b3d18c35332
darkkseer <92098618+darkkseer@users.noreply.github.com>
2023-06-21 18:24:06 +0300

    Add files via upload
    network

---
+++ gitCommit/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            buffer: 'buffer',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/js/pdf.worker.js", to: "." },
+                { from: "src/js/pdf.worker.js.map", to: "." },
+                { from: "index.html", to: "." },
+                { from: "node_modules/tesseract.js-core/tesseract-core.wasm.js", to: "." },
+            ],
+        }),
+    ],
+};
